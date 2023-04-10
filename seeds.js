ace7bbe7fe93d5526946170b9e5283159c2a2632
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-04-09 19:54:31 -0700

    added seed data

--- seeds.js
+++ seeds.js
@@ -11,13 +11,49 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand', {useNewUrlParser: true,u
 })
 
 
-const p = new Product({
-    name: "Ruby Grapefruit",
-    price: 1.99,
-    category: "fruit"
-})
+// const p = new Product({
+//     name: "Ruby Grapefruit",
+//     price: 1.99,
+//     category: "fruit"
+// })
 
-p.save().then(p=>{
-    console.log(p)
+// p.save().then(p=>{
+//     console.log(p)
+// })
+// .catch(e=>console.log(e))
+
+
+const seedProducts = [
+    {
+        name: "Fairy Eggplant ",
+        price: 1.00,
+        category: "vegetable"
+    },
+    {
+        name: "Organic Goddess Melon",
+        price: 4.99,
+        category: "fruit"
+    },
+    {
+        name: "Organic Mini Seedless Watermelon",
+        price: 3.99,
+        category: "fruit"
+    },
+    {
+        name: "Organic Celery",
+        price: 1.50,
+        category: "vegetable"
+    },
+    {
+        name: "Chocolate Whole Milk",
+        price: 2.69,
+        category: "Dairy"
+    }
+]
+Product.insertMany(seedProducts)
+.then(res=>{
+    console.log(res)
 })
-.catch(e=>console.log(e))
+.catch(e=>{
+    console.log(e)
+})
