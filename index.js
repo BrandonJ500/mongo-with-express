60d6d36fa3d6a4d8b17bcaa3122659decf669916
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-04-23 19:48:12 -0700

    added error function

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const mongoose = require('mongoose');
 const AppError = require("./AppError");
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("connected")
-    })
-    .catch(err => {
-        console.log("Error", err)
-    })
+.then(() => {
+    console.log("connected")
+})
+.catch(err => {
+    console.log("Error", err)
+})
 
 
 
@@ -26,90 +26,75 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"));
 
-app.get("/products", async (req, res, next) => {
-    try{
-          const { category } = req.query;
-    if (category) {
-        const products = await Product.find({ category })
-
-        res.render("products/index", { products, category })
-    } else {
-        const products = await Product.find({})
-
-        res.render("products/index", { products, category: "All" })
-    }  
-    }catch (e){
-       next(e);
+function wrapAsyc(fn){
+    return function(req,res, next){
+        fn(req,res,next).catch(e=>next(e));
     }
+}
 
-})
+
+app.get("/products", wrapAsync( async (req, res, next) => {
+        const { category } = req.query;
+        if (category) {
+            const products = await Product.find({ category })
+
+            res.render("products/index", { products, category })
+        } else {
+            const products = await Product.find({})
+
+            res.render("products/index", { products, category: "All" })
+        }
+}))
 
 app.get("/products/new", (req, res) => {
     res.render("products/new", { categories })
 })
 
-app.post("/products", async (req, res, next) => {
-    try {
+app.post("/products", wrapAsync(async (req, res, next) => {
+
         const newProduct = new Product(req.body)
         await newProduct.save();
         res.redirect("/products")
-    }
-    catch (e) {
-        next(new AppError("Something went wrong"), 404)
-    }
+}))
 
-})
 
-app.get("/products/:id", async (req, res, next) => {
-    try {
-           const { id } = req.params;
+app.get("/products/:id", wrapAsyc( async (req, res, next) => {
+
+    const { id } = req.params;
     const product = await Product.findById(id);
     if (!product) {
         throw new AppError("Product Not Found", 404)
     }
-    res.render("products/show", { product }) 
-    }catch(e){
-        next(e);
-    }
+    res.render("products/show", { product })
+}))
 
-})
+app.get("/products/:id/edit", wrapAsync(async (req, res, next) => {
+ 
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            throw new AppError("Product Not Found", 404)
+        }
+        res.render("products/edit", { product, categories })
+}))
 
-app.get("/products/:id/edit", async (req, res, next) => {
-    try {
-            const { id } = req.params;
-    const product = await Product.findById(id);
-    if (!product) {
-        throw new AppError("Product Not Found", 404)
-    }
-    res.render("products/edit", { product, categories })
-    } catch (e){
-        next(e)
-    }
-
-})
-
-app.put("/products/:id", async (req, res, next) => {
-    try {
+app.put("/products/:id", wrapAsync(wrapAsync(async (req, res, next) => {
         const { id } = req.params
         const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
         res.redirect(`/products/${product._id}`)
-    } catch (e) {
-        next(e)
-    }
 
-})
+})))
 
-app.delete("/products/:id", async (req, res) => {
+app.delete("/products/:id", wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     console.log(req.params)
     const productDelete = await Product.findByIdAndDelete(id);
     productDelete;
     res.redirect("/products")
-})
+}))
 
 app.use((err, req, res, next) => {
     const { status = 500, message = "something went wrong" } = err;
-
     res.status(status).send(message);
 })
 
